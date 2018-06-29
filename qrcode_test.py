b3d869082c5db6212278db47cae713cc4d420172
Yejin Han <nekoto96@gmail.com>
2018-06-29 10:50:24 +0900

    qrcode: Put json data

--- qrcode_test.py
+++ qrcode_test.py
@@ -1,5 +1,8 @@
 import qrcode
-
+import json
+import urllib.request
+import datetime
+data = 0
 qr = qrcode.QRCode(
     version = 1,
     error_correction = qrcode.constants.ERROR_CORRECT_H,
@@ -7,11 +10,18 @@ qr = qrcode.QRCode(
     border=4,
 )
 
-data = "Hello!"
+now = datetime.datetime.now()
 
-qr.add_data(data)
+def init(vinsureType):
+    global data
+    data = {'insureType':vinsureType}
+
+
+json_data = json.dumps(data)
+qr.add_data(json_data)
 qr.make(fit = True)
 
 img = qr.make_image()
-
-img.save("qr.jpg")
+nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+imgname = "qrcode_"+nowDatetime+".jpg"
+img.save(imgname)
